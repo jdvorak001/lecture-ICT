40b3e1798a8242223d3023988d3743ca5643b4f6
jdvorak <jdvorak@dvorak-ii.vc.cvut.cz>
2018-05-31 08:53:13 +0200

    the fourth problem

---
+++ problem-004/get-and-process-data.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# dataset from http://data.europa.eu/euodp/en/data/dataset/cordisH2020projects, the "H2020 Organizations" in CSV format
+wget --no-verbose --timestamping --directory-prefix=data/ http://cordis.europa.eu/data/cordis-h2020organizations.csv
+
+# ???
+cat data/cordis-h2020organizations.csv | \
+sed 1d | \
+grep ';coordinator;' | \
+cut -f2,6,11 -d';' | \
+sort > data/c.txt
+
+# ???
+cat data/cordis-h2020organizations.csv | \
+sed 1d | \
+grep -v ';coordinator;' | \
+cut -f2,6,11 -d';' | \
+sort > data/p.txt
+
+# ???
+join -t';' data/c.txt data/p.txt | \
+cut -f2-5 -d';' | \
+sort | \
+uniq -c | \
+sort -n -r | \
+sed -e 's/^ *\([0-9]*\) */\1;/' > data/m1.txt
+
+# ???
+join -t';' data/c.txt data/p.txt | \
+cut -f3,5 -d';' | \
+sort | \
+uniq -c | \
+sort -n -r | \
+sed -e 's/^ *\([0-9]*\) */\1;/' > data/m2.txt
